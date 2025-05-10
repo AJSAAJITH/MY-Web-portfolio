de7197d8873084d7710e45f0e55137b61e4bc46a
AJA Saajith <ajsajith99@gmail.com>
2025-05-11 00:00:44 +0530

    update resume update

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -22,6 +22,7 @@ function Resume() {
                         href="https://drive.google.com/file/d/1fyHz3bc7WFlRmAb4ezzW5KAXZePRv5YD/view?usp=drive_link" // Replace with the actual file path or URL
                         download="AJASaajith_CV.pdf"
                         className="p-4 text-sm font-semibold tracking-tighter text-purple-200 bg-neutral-500 rounded-2xl"
+                        target="_blank"
                     >
                         Resume
                     </a>
